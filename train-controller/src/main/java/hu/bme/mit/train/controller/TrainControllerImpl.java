f393b3bd80b0529a5333924b152c6784e3a021d3
lavatilivi <lavatil@gmail.com>
2017-02-27 08:29:12 -0800

    techograph

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -1,12 +1,17 @@
 package hu.bme.mit.train.controller;
+import com.google.common.collect.Table;
+import com.google.common.collect.HashBasedTable;
 
 import hu.bme.mit.train.interfaces.TrainController;
+
+import java.util.Date;
 
 public class TrainControllerImpl implements TrainController {
 
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+
 
 	@Override
 	public void followSpeed() {
@@ -40,6 +45,13 @@ public class TrainControllerImpl implements TrainController {
 	@Override
 	public void setJoystickPosition(int joystickPosition) {
 		this.step = joystickPosition;		
+	}
+
+	public Table tachograph(){
+		Table<Long,Integer, Integer> tacho = HashBasedTable.create();
+		Date date = new Date();
+		tacho.put(date.getTime(),this.step, referenceSpeed);
+		return tacho;
 	}
 
 }
